c3eaf22f09e1ad82ac46cfef2c41356a01157382
Dustin <dhendrik6@avans.nl>
2019-12-12 10:52:52 +0100

    Waypoint class added

--- app/src/main/java/LogicTier/RouteManager/Route/Waypoint.java
+++ app/src/main/java/LogicTier/RouteManager/Route/Waypoint.java
@@ -1,4 +1,94 @@
 package LogicTier.RouteManager.Route;
 
+import java.util.ArrayList;
+
 public class Waypoint {
+    private boolean hasBeenChecked;
+    private boolean isVisitedChecked;
+    private int id;
+    private String name;
+    private String latitude;
+    private String longitude;
+    private double height;
+    private ArrayList<String> multimedia;
+
+    public boolean isHasBeenChecked()
+    {
+        return hasBeenChecked;
+    }
+
+    public void setHasBeenChecked(boolean hasBeenChecked)
+    {
+        this.hasBeenChecked = hasBeenChecked;
+    }
+
+    public boolean isVisitedChecked()
+    {
+        return isVisitedChecked;
+    }
+
+    public void setVisitedChecked(boolean visitedChecked)
+    {
+        isVisitedChecked = visitedChecked;
+    }
+
+    public int getId()
+    {
+        return id;
+    }
+
+    public void setId(int id)
+    {
+        this.id = id;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    public String getLatitude()
+    {
+        return latitude;
+    }
+
+    public void setLatitude(String latitude)
+    {
+        this.latitude = latitude;
+    }
+
+    public String getLongitude()
+    {
+        return longitude;
+    }
+
+    public void setLongitude(String longitude)
+    {
+        this.longitude = longitude;
+    }
+
+    public double getHeight()
+    {
+        return height;
+    }
+
+    public void setHeight(double height)
+    {
+        this.height = height;
+    }
+
+    public ArrayList<String> getMultimedia()
+    {
+        return multimedia;
+    }
+
+    public void setMultimedia(ArrayList<String> multimedia)
+    {
+        this.multimedia = multimedia;
+    }
 }
