09e3e179e3e8ab4e5c23b19268f5eb7aac991c46
Chiem <Chiem.stevens@outlook.com>
2020-01-08 10:04:36 +0100

    Fix the database crash

--- app/src/main/java/PresentationTier/MainActivity/MainActivity.java
+++ app/src/main/java/PresentationTier/MainActivity/MainActivity.java
@@ -20,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
         databaseManager.insertWaypointIntoDB(new Waypoint(false, false, "VVV", "51.588762", "4.776913", 0, null));
         databaseManager.insertWaypointIntoDB(new Waypoint(false, false, "De Bruine Pij", "51.588791", "4.775477", 0, null));
         databaseManager.insertWaypointIntoDB(new Waypoint(false, false, "Corenmaet", "51.589308", "4.774484", 0, null));
-        databaseManager.insertWaypointIntoDB(new Waypoint(false, false, "O'Mearas Irish Pub", "51.589448", "4.775846", 0, null));
+        databaseManager.insertWaypointIntoDB(new Waypoint(false, false, "OMearas Irish Pub", "51.589448", "4.775846", 0, null));
         databaseManager.insertWaypointIntoDB(new Waypoint(false, false, "Kasteel van Breda", "51.590504", "4.776221", 0, null));
         databaseManager.insertWaypointIntoDB(new Waypoint(false, false, "Café Publieke Werken", "51.588973", "4.778062", 0, null));
 
